7c60603a2c67783360229ae10add510c267e2521
doquang0310 <38870227+doquang0310@users.noreply.github.com>
2023-06-08 21:13:07 +0700

    update endpoint

--- src/ultils/endpoint.ts
+++ src/ultils/endpoint.ts
@@ -1,5 +1,5 @@
 export const ENDPOINT = {
-    LOGIN: `${process.env.REACT_APP_BASE_API_URL}/auth/login`,
-    CREATE_VIDEO: `${process.env.REACT_APP_BASE_API_URL}/videos`,
-    FETCH_VIDEOS: `${process.env.REACT_APP_BASE_API_URL}/videos`,
+    LOGIN: `${process.env.REACT_APP_BASE_API_URL}auth/login`,
+    CREATE_VIDEO: `${process.env.REACT_APP_BASE_API_URL}videos`,
+    FETCH_VIDEOS: `${process.env.REACT_APP_BASE_API_URL}videos`,
 }
